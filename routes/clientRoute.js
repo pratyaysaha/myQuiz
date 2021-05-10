07308603f32cd229ea32bb8533e6ec93463ff3b3
Swagata Chanda <swagata.a2000chanda@gmail.com>
2021-05-11 00:11:49 +0530

    Auto email generated

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const QuizDetails= require('../models/quizdetails')
 const Questions=require('../models/questions')
+const nodemailer = require('nodemailer')
+const Submit = require('../models/submission');
+const fetch = require('node-fetch');
+require('dotenv/config')
 const router= express.Router()
 
 
@@ -27,8 +31,51 @@ router.get('/:quizid',async (req,res)=>{
 router.get('/:quizname',async(req, res)=>{
 
 })
+
+
+
 router.get('/:submissionId/success' ,async(req, res)=>{
     res.render('success')
+    try{
+       
+        
+       await fetch(`http://localhost:3000/api/evaluate/submission/${req.params.submissionId}`)
+            
+        .then(res => res.json())
+        .then(json => console.log(json))
+    
+        const submits = await Submit.find({ _id: req.params.submissionId })
+        console.log(submits)
+        console.log(submits[0].Email)
+
+
+    var transport = nodemailer.createTransport({
+        host : 'smtp.gmail.com',
+        port: 465,
+        secure : true,
+        auth: {
+           user: 'startechchannel@gmail.com',
+           pass: `${process.env.EMAIL_PASS}`
+        }
+    });
+    const message = {
+        from: 'startechchannel@gmail.com', 
+        to: submits[0].Email,         
+        subject: 'Submission Succesfull', 
+        text: `Total Score: ${submits[0].totalMarks}`
+    };
+    transport.sendMail(message, function(err, info) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(info);
+        }
+    });
+}
+catch(err){
+    console.log(err)
+}
+
 })
 
 
