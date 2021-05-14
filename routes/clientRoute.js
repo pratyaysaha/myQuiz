4b08457ea86bf9de88bff6eb9b4c878d262dc627
swagatachanda <swagata.chanda2000@gmail.com>
2021-05-14 08:53:36 +0530

    Small changes

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -37,22 +37,13 @@ router.get('/:quizid',async (req,res)=>{
         if(Date.now()>=quizDetails.stime&&Date.now()<=quizDetails.etime)
         {
            
-<<<<<<< HEAD
-            res.render("quizpage",{data : {quizDetails, questions: sendQuestion}})
+            res.render("quizpage",{data : {quizDetails, questions: sendQuestion}, timelimit: true})
        
         }
         else{
              res.send("Quiz not available")
          }
     }
-=======
-            res.render("quizpage",{data : {quizDetails, questions: sendQuestion},timelimit: true})
-        }  
-        else{
-            res.send("Quiz not available")
-        }
-}
->>>>>>> c3b7894d3b4c72cac90f515ba31a983d7d7f588e
     catch(err)
     {
         console.log("err");
